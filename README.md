417d41738f234bd3f0aa374f6cb64b9d7bb006b1
Caleb Helbling <caleb.helbling@yahoo.com>
2020-10-03 22:20:44 -0500

    Added alternative implementation that doesn't suffer from gradient vanishing

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Differentiable SHA256
 A fully differentiable implementation of SHA256. Unfortunately suffers from gradient vanishing.
 
+Update: an alternative implementation without the gradient vanishing issue was implemented. The gradients are no longer zero, however the surface of the output hash is rife with local minima, likely because of the avalanche effect. It appears that SHA256 is secure from gradient descent pre-image attacks.
+
 # Dependencies
 * PyTorch
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import torch
 import random
 import hashlib
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def numerically_stable_sigmoid(x):
+    pos_mask = x >= 0
+    neg_mask = ~pos_mask
+    z = torch.zeros_like(x)
+    z[pos_mask] = torch.exp(-x[pos_mask])
+    z[neg_mask] = torch.exp(x[neg_mask])
+    top = torch.ones_like(x)
+    top[neg_mask] = z[neg_mask]
+    return top / (1.0 + z)
 
 def and_bit(a, b):
     return a * b
@@ -36,6 +49,51 @@ def add(a, b):
         (sum_i, carry) = full_adder(a_bit, b_bit, carry)
         ret.insert(0, sum_i)
 
+    return ret
+
+def add_num(a, b, num_bits):
+    sum = a + b
+    cond = numerically_stable_sigmoid(sum - 2 ** num_bits)
+    return cond * (sum - 2 ** num_bits) + (1.0 - cond) * sum
+
+def add_num32(a, b):
+    return add_num(a, b, 32)
+
+def num_to_bits_differentiable(x, num_bits):
+    ret = []
+    y = x / (2 ** (num_bits - 1))
+    for _ in range(num_bits):
+        bit = numerically_stable_sigmoid(50.0 * (y - 1.0))
+        remainder = y - bit
+        y = remainder * 2.0
+        ret.append(bit)
+    return ret
+
+def triangle_wave(x, delta=0.01):
+    return 1.0 - 2.0 * torch.acos((1.0 - delta) * torch.sin(2.0 * math.pi * x)) / math.pi
+
+def square_wave(x, delta=0.01):
+    return 2.0 * torch.atan(torch.sin(2.0 * math.pi * x) / delta) / math.pi
+
+def sawtooth_wave(x, delta=0.01):
+    return (1 + triangle_wave((2.0 * x - 1.0) / 4.0, delta=delta) * square_wave(x / 2.0, delta=delta)) / 2.0
+
+def floor_(x, delta=0.01):
+    return x - sawtooth_wave(x, delta=delta)
+
+def mod_(x, n, delta=0.01):
+    return n * sawtooth_wave(x / n, delta=delta)
+
+def mod2_alt(x):
+    return (torch.sin((x - 0.5) * math.pi) + 1) / 2.0
+
+def num_to_bits_differentiable2(x, num_bits):
+    ret = []
+    for i in reversed(range(num_bits)):
+        y = x / (2 ** i)
+        right_shifted = torch.max(torch.tensor(0.0), floor_(y))
+        bit = mod2_alt(right_shifted)
+        ret.append(bit)
     return ret
 
 def not_(a):
@@ -101,7 +159,8 @@ parallel_zeros = torch.tensor([0.0]).expand(parallel_tries)
 parallel_ones = torch.tensor([1.0]).expand(parallel_tries)
 
 def expand_constant(n):
-    return [parallel_zeros if bit == 0 else parallel_ones for bit in n]
+    #return [parallel_zeros if bit == 0 else parallel_ones for bit in n]
+    return [torch.tensor([0.0]).expand(parallel_tries) if bit == 0 else torch.tensor([1.0]).expand(parallel_tries) for bit in n]
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
@@ -109,6 +168,7 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 def sha256(message):
+    """
     h0 = expand_constant(num_to_32_bits(h0n))
     h1 = expand_constant(num_to_32_bits(h1n))
     h2 = expand_constant(num_to_32_bits(h2n))
@@ -117,6 +177,15 @@ def sha256(message):
     h5 = expand_constant(num_to_32_bits(h5n))
     h6 = expand_constant(num_to_32_bits(h6n))
     h7 = expand_constant(num_to_32_bits(h7n))
+    """
+    h0 = torch.tensor(float(h0n)).expand(parallel_tries)
+    h1 = torch.tensor(float(h1n)).expand(parallel_tries)
+    h2 = torch.tensor(float(h2n)).expand(parallel_tries)
+    h3 = torch.tensor(float(h3n)).expand(parallel_tries)
+    h4 = torch.tensor(float(h4n)).expand(parallel_tries)
+    h5 = torch.tensor(float(h5n)).expand(parallel_tries)
+    h6 = torch.tensor(float(h6n)).expand(parallel_tries)
+    h7 = torch.tensor(float(h7n)).expand(parallel_tries)
 
     k = [expand_constant(num_to_32_bits(n)) for n in kn]
 
@@ -136,27 +205,30 @@ def sha256(message):
     message = message + [parallel_zeros] * K
 
     # append L as a 64-bit big-endian integer, making the total post-processed length a multiple of 512 bits
-    message = message + num_to_64_bits(L)
+    message = message + [torch.tensor(bit, dtype=torch.float) for bit in num_to_64_bits(L)]
 
     for chunk in chunks(message, 512):
         w = list(chunks(chunk, 32)) + [None] * (64 - 16)
+        w_num = [bits_to_num(n) for n in chunks(chunk, 32)] + [None] * (64 - 16)
         for i in range(16, 64):
             # s0 := (w[i-15] rightrotate  7) xor (w[i-15] rightrotate 18) xor (w[i-15] rightshift  3)
             s0 = xor(xor(right_rotate(w[i - 15], 7), right_rotate(w[i - 15], 18)), right_shift(w[i - 15], 3))
             # s1 := (w[i- 2] rightrotate 17) xor (w[i- 2] rightrotate 19) xor (w[i- 2] rightshift 10)
             s1 = xor(xor(right_rotate(w[i - 2], 17), right_rotate(w[i - 2], 19)), right_shift(w[i - 2], 10))
             # w[i] := w[i-16] + s0 + w[i-7] + s1
-            w[i] = add(add(add(w[i - 16], s0), w[i - 7]), s1)
+            w_num[i] = add_num32(add_num32(add_num32(w_num[i-16], bits_to_num(s0)), w_num[i-7]), bits_to_num(s1))
+            w[i] = num_to_bits_differentiable2(w_num[i], 32)
+            #w[i] = add(add(add(w[i - 16], s0), w[i - 7]), s1)
 
         # Initialize working variables to current hash value:
-        a = h0
-        b = h1
-        c = h2
-        d = h3
-        e = h4
-        f = h5
-        g = h6
-        h = h7
+        a = num_to_bits_differentiable2(h0, 32)
+        b = num_to_bits_differentiable2(h1, 32)
+        c = num_to_bits_differentiable2(h2, 32)
+        d = num_to_bits_differentiable2(h3, 32)
+        e = num_to_bits_differentiable2(h4, 32)
+        f = num_to_bits_differentiable2(h5, 32)
+        g = num_to_bits_differentiable2(h6, 32)
+        h = num_to_bits_differentiable2(h7, 32)
 
         # Compression function main loop:
         for i in range(0, 64):
@@ -165,34 +237,38 @@ def sha256(message):
             # ch := (e and f) xor ((not e) and g)
             ch = xor(and_(e, f), and_(not_(e), g))
             # temp1 := h + S1 + ch + k[i] + w[i]
-            temp1 = add(add(add(add(h, S1), ch), k[i]), w[i])
+            #temp1 = add(add(add(add(h, S1), ch), k[i]), w[i])
+            temp1 = add_num32(add_num32(add_num32(add_num32(bits_to_num(h), bits_to_num(S1)), bits_to_num(ch)), bits_to_num(k[i])), w_num[i])
             # S0 := (a rightrotate 2) xor (a rightrotate 13) xor (a rightrotate 22)
             S0 = xor(xor(right_rotate(a, 2), right_rotate(a, 13)), right_rotate(a, 22))
             # maj := (a and b) xor (a and c) xor (b and c)
             maj = xor(xor(and_(a, b), and_(a, c)), and_(b, c))
             # temp2 := S0 + maj
-            temp2 = add(S0, maj)
+            #temp2 = add(S0, maj)
+            temp2 = add_num32(bits_to_num(S0), bits_to_num(maj))
 
             h = g
             g = f
             f = e
-            e = add(d, temp1)
+            #e = add(d, temp1)
+            e = num_to_bits_differentiable2(add_num32(bits_to_num(d), temp1), 32)
             d = c
             c = b
             b = a
-            a = add(temp1, temp2)
+            #a = add(temp1, temp2)
+            a = num_to_bits_differentiable2(add_num32(temp1, temp2), 32)
 
         # Add the compressed chunk to the current hash value:
-        h0 = add(h0, a)
-        h1 = add(h1, b)
-        h2 = add(h2, c)
-        h3 = add(h3, d)
-        h4 = add(h4, e)
-        h5 = add(h5, f)
-        h6 = add(h6, g)
-        h7 = add(h7, h)
+        h0 = add_num32(h0, bits_to_num(a))
+        h1 = add_num32(h1, bits_to_num(b))
+        h2 = add_num32(h2, bits_to_num(c))
+        h3 = add_num32(h3, bits_to_num(d))
+        h4 = add_num32(h4, bits_to_num(e))
+        h5 = add_num32(h5, bits_to_num(f))
+        h6 = add_num32(h6, bits_to_num(g))
+        h7 = add_num32(h7, bits_to_num(h))
 
-    return h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7
+    return [h0, h1, h2, h3, h4, h5, h6, h7]
 
 hello_world = "Hello world"
 hello_world_hash = 0x64ec88ca00b268e5ba1a35678a1b5316d212f4f366b2477232534a8aeca37f3c
@@ -200,7 +276,46 @@ hello_world_ascii = [ord(c) for c in hello_world]
 hello_world_binary = flatten([num_to_8_bits(c) for c in hello_world_ascii])
 assert(len(hello_world_binary) % 8 == 0)
 hello_world_torch = expand_constant(hello_world_binary)
-hello_world_digest = sha256(hello_world_torch)
+
+samples = 10
+x = np.linspace(0, 1, samples)
+y = np.linspace(0, 1, samples)
+
+z_output = np.zeros((samples, samples))
+
+for j in range(samples):
+    for i in range(samples):
+        hello_world_torch[0][0] = x[i]
+        hello_world_torch[1][0] = y[j]
+        hello_world_digest = sha256(hello_world_torch)
+
+        #leading_zeros = 32
+        #hw_leading_digest = hello_world_digest[0:leading_zeros]
+        #hw_leading_float = float(bits_to_num(hw_leading_digest)[0])
+        hw_leading_float = float(hello_world_digest[0])
+
+        z_output[i, j] = float(hello_world_digest[0])
+
+        print("{" + str(x[i]) + "," + str(y[j]) + "," + str(hw_leading_float) + "},")
+
+print(z_output)
+
+input("Continue?")
+
+x_grid, y_grid = np.meshgrid(x, y)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(x_grid, y_grid, z_output, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
+
+input("Continue?")
+
+
 
 bits = [round(float(bit[0])) for bit in hello_world_digest]
 bits_string = ''.join(['0' if b == 0 else '1' for b in bits])
@@ -223,7 +338,7 @@ raw_nonce = [torch.zeros(parallel_tries, requires_grad=True) for _ in range(32)]
 prev_hash_py = [random.choice([0, 1]) for _ in range(256)]
 prev_hash = expand_constant(prev_hash_py)
 
-optimizer = torch.optim.SGD(raw_nonce, lr=10.0, momentum=0.9)
+optimizer = torch.optim.SGD(raw_nonce, lr=0.1, momentum=0.9)
 
 def normalize(input, dim):
     norm = torch.norm(input, dim=dim, keepdim=True)
@@ -254,7 +369,7 @@ while True:
     normalized_stacked_grad = normalize(stacked_grad, dim=0)
 
     for (i, raw_nonce_bit) in enumerate(raw_nonce):
-        raw_nonce_bit.data -= normalized_stacked_grad[i]
+        raw_nonce_bit.data -= normalized_stacked_grad[i] * 0.1
 
     if input("Stop computation?") == "y":
         break
